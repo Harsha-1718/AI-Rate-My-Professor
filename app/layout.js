24fe49b3b0c48c976ca3a0138209f34d03dd611b
Harsha Vardhan Reddy <hvardhan1718@gmail.com>
2024-08-23 19:11:02 -0500

    updated authcontext for dashboard

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,19 @@
-// app/layout.js
-'use client'
-import { AuthProvider } from './context/authContext';
+'use client';
+import { AuthProvider } from './context/authContext'; // Ensure this path is correct
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Navbar from './navbar';
-import Dashboard from './dashboard/page';
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <AuthProvider>
-         
-            <CssBaseline />
-            <Navbar/>
-            <main>
-              {children} {/* This renders the content of each page */}
-            </main>
-
-
+          <CssBaseline />
+          <Navbar />
+          <main>
+            {children} {/* This renders the content of each page */}
+          </main>
         </AuthProvider>
       </body>
     </html>
